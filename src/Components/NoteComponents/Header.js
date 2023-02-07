faedf7b1d56384498c1e6c36617754333c997275
Chanbin Park <31779041+davidinthepark@users.noreply.github.com>
2023-02-06 19:25:10 -0500

    Initial commit

---
+++ src/Components/NoteComponents/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="header">
+            <h1 className="notes__title">React Notes App</h1>
+        </div>
+    );
+}
+export default Header;
