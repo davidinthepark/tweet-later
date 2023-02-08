abb019cdb6cb98ae4aea713a19daea681052e01b
Chanbin Park <31779041+davidinthepark@users.noreply.github.com>
2023-02-08 16:10:50 -0500

    Share functionality added

--- src/Components/NoteComponents/Note.js
+++ src/Components/NoteComponents/Note.js
@@ -1,18 +1,38 @@
 import React from "react";
 import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
+import EditIcon from '@mui/icons-material/Edit';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 function Note({ id, text, deleteNote }) {
-    return (
-      <div className="note">
-        <div className="note__body">{text}</div>
-        <div className="note__footer" style={{ justifyContent: "flex-end" }}>
-          <DeleteForeverOutlinedIcon
+  const textParsed = encodeURIComponent(text.trim());
+  const twitterURL = "https://twitter.com/intent/tweet?url=&text=" + textParsed + "&via=";
+  
+  const shareTweet = () => {
+    window.open(twitterURL, '_blank').focus();
+  }
+  
+  return (
+    <div className="note">
+      <div className="note__body" id="note__body">{text}</div>
+      <div className="note__footer" style={{ justifyContent: "flex-end" }}>
+        {/* <EditIcon
+            className="note__delete"
+            onClick={() => editNote(id)}
+            aria-hidden="true"
+          ></EditIcon> */}
+
+        <DeleteForeverOutlinedIcon
             className="note__delete"
             onClick={() => deleteNote(id)}
             aria-hidden="true"
           ></DeleteForeverOutlinedIcon>
-        </div>
+        <TwitterIcon
+          className="note__share"
+          onClick={() => shareTweet()}
+          aria-hidden="true"
+        ></TwitterIcon>
       </div>
-    );
-  }
+    </div>
+  );
+}
 export default Note;
