26f9d0e8dc8dfbebdfff63402f34ba436d3ece55
Chanbin Park <31779041+davidinthepark@users.noreply.github.com>
2023-02-08 16:26:20 -0500

    App rebranded

--- src/Components/NoteComponents/Header.js
+++ src/Components/NoteComponents/Header.js
@@ -1,9 +1,18 @@
 import React from "react";
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 function Header() {
     return (
         <div className="header">
-            <h1 className="notes__title">React Notes App</h1>
+            <h1 className="notes__title">
+                <div className="header_icon">
+                    <TwitterIcon
+                        aria-hidden="true"
+                        sx={{ fontSize: 33}}
+                    ></TwitterIcon>
+                </div>
+                Tweet Later
+            </h1>
         </div>
     );
 }
